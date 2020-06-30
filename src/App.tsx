1e7eeb6f07c0a76798ba35c5ce1281f4e12860a5
Jørgen Tvedt <tvedtorama@gmail.com>
2020-06-30 07:00:57 +0200

    Very basic react router setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import { ReadQr } from './ReadQr';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/read-qr">Scan QR</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/read-qr">
+            <ReadQr />
+          </Route>
+          <Route path="/">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
       </header>
     </div>
   );
---
+++ src/ReadQr.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const ReadQr = ()=> <div>
+	<span>hello read!</span>
+</div>
