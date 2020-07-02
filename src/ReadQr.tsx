6378e7821ee0030c2b745a22d8ffa3333cf8ba6e
Jørgen Tvedt <tvedtorama@gmail.com>
2020-07-02 07:57:09 +0200

    Fixed issue with missing update of animation request.

--- src/ReadQr.tsx
+++ src/ReadQr.tsx
@@ -9,14 +9,15 @@ export const ReadQr = () => Some({
 		stateThings: useState<"INIT" | "WAIT" | "FAILED" | "STREAMING">("INIT"),
 		videoRef: useRef<HTMLVideoElement>(null),
 		canvasRef: useRef<HTMLCanvasElement>(null),
-		animRef: useRef<number>(0),
-	}).map(({animRef, videoRef, canvasRef, ...rest}) => ({
+	}).map(({videoRef, canvasRef, ...rest}) => ({
 		nothing: React.useEffect(() => {
 			if (videoRef.current == null || canvasRef.current == null) {
 				throw new Error("This handling is here because of strict mode, the variable defs below as well")
 			}
 			const videoElm = videoRef.current
 			const canvasElm = canvasRef.current
+			let animRequest = 0
+			const reqAnim = () => animRequest = requestAnimationFrame(animate)
 			const animate = () => {
 				if (videoElm.readyState === videoElm.HAVE_ENOUGH_DATA) {
 					Maybe.fromFalsy(canvasElm.getContext("2d")).
@@ -24,7 +25,7 @@ export const ReadQr = () => Some({
 							ctx.drawImage(videoElm, 0, 0, canvasElm.width, canvasElm.height);
 						})	
 				}
-				requestAnimationFrame(animate);
+				reqAnim()
 			}
 			if (rest.stateThings[0] === "INIT") {
 				rest.stateThings[1]("WAIT")
@@ -34,20 +35,20 @@ export const ReadQr = () => Some({
 						videoElm.srcObject = stream;
 						videoElm.setAttribute("playsinline", "true"); // required to tell iOS safari we don't want fullscreen
 						videoElm.play();
-						animRef.current = requestAnimationFrame(animate);
+						reqAnim()
 						rest.stateThings[1]("STREAMING")
 					})).catch(err => {
 						rest.stateThings[1]("FAILED")
 					})
 			}
 		  
-			return () => animRef.current && cancelAnimationFrame(animRef.current) || undefined;
+			return () => animRequest && cancelAnimationFrame(animRequest) || undefined;
 		  }, []),
 		...rest,
 		videoRef,
 		canvasRef,
 	})).
-	map(({stateThings: [state, setState], videoRef, canvasRef}) => 
+	map(({stateThings: [state], videoRef, canvasRef}) => 
 		state === "FAILED" ?
 			<div style={{color: "orange", margin: "2em"}}>🎥 Unable to access video stream (please make sure you have a webcam enabled)</div> :
 			<div style={{display: "flex", flexDirection: "column"}}>
@@ -55,5 +56,5 @@ export const ReadQr = () => Some({
 				{ /* Insane error here: used style (CSS) width/height, instead of plain w/h.  This led to some crazy scaling that was impossible to understand.  Look up "300x150" for an explanation. 
 					Note: Assumes 640x480 is same aspect as video stream, if this assumption fails, the result will be stretched */ }
 				<canvas ref={canvasRef} width={`${640}px`} height={`${480}px`} />
-				<span>hello read! {state}</span>
+				<span>Reader, state: {state}</span>
 			</div>).some()
